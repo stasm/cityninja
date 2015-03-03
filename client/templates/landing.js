4f64e950beb31af0c854c87a829c22e3d903f91c
Staś Małolepszy <stas@mozilla.com>
2015-03-03 21:52:26 +0100

    Facebook app id

--- client/templates/landing.js
+++ client/templates/landing.js
@@ -1,14 +1,18 @@
 Template.landing.rendered = function() {
-  // Twitter button script
-  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1615730801984225',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
 
-  // Facebook button script
-  (function(d, s, id) {
-      var js;
-      var fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=504450969581453&version=v2.0';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 };
