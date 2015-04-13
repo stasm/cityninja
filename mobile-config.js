f7e5d0a4ac136b42d42232db2ae0ae6d0b735e59
Staś Małolepszy <stas@mozilla.com>
2015-04-14 00:56:15 +0200

    Add mobile-config.js for Android

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'warszawski.ninja',
+  description: 'Problem z kursowaniem metra? Zgłoś go! Nie daj zaskoczyć się opóźnieniom.',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'public/android-chrome-36x36.png',
+  'android_mdpi': 'public/android-chrome-48x48.png',
+  'android_hdpi': 'public/android-chrome-72x72.png',
+  'android_xhdpi': 'public/android-chrome-96x96.png',
+});
