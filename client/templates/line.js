accbe93ee9fd4c0a836a8811b506134ecb81b9fb
Staś Małolepszy <stas@mozilla.com>
2015-05-17 19:42:15 +0200

    subheader

--- client/templates/line.js
+++ client/templates/line.js
@@ -3,13 +3,6 @@ Template.line.onCreated(function() {
 });
 
 Template.line.helpers({
-  noFavs: function() {
-    return !Session.get('favs count');
-  },
-  isFav: function(line, dir) {
-    // if no line is a fav, all are
-    return !Session.get('favs count') || Session.get('fav ' + line + dir);
-  },
   badReportCategories: function(type, line, dir) {
     return reportCategories.filter(function(category) {
       if (category.name === 'normal') {
