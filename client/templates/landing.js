2de5e3c9ff2accb9430b90f91934f6d04117cd0d
Staś Małolepszy <stas@mozilla.com>
2015-05-18 11:03:31 +0200

    fix links

--- client/templates/landing.js
+++ client/templates/landing.js
@@ -1,17 +1,3 @@
-Template.landing.rendered = function() {
-
-  $(".collapsible-header").click(function() {
-    $(this).toggleClass('active')
-      .parent().toggleClass('active');
-  });
-
-  // Make whole div with line clickable
-  $(".collapsible-body").click(function() {
-    window.location = $(this).find("a").attr("href");
-    return false;
-  });
-};
-
 Template.landing.helpers({
   lines: function(lineType) {
     return lines[lineType];
--- client/templates/line.js
+++ client/templates/line.js
@@ -1,6 +1,16 @@
-Template.line.onCreated(function() {
-  this.subscribe('reports');
-});
+Template.landing.rendered = function() {
+
+  $(".collapsible-header").click(function() {
+    $(this).toggleClass('active')
+      .parent().toggleClass('active');
+  });
+
+  // Make whole div with line clickable
+  $(".collection-item").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
+};
 
 Template.line.helpers({
   hasBadReports: function(type, line, dir) {
