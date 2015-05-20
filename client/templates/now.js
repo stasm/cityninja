d216d206bf0136d3f5531d0891c79d6e78ed3a75
Staś Małolepszy <stas@mozilla.com>
2015-05-20 09:25:37 +0200

    add touch interaction on the cover screen

--- client/templates/now.js
+++ client/templates/now.js
@@ -25,3 +25,134 @@ Template.now.helpers({
   },
   reports: getReports
 });
+
+Template.now.onRendered(function() {
+
+  var reqAnimationFrame = (function () {
+    var raf =  window[Hammer.prefixed(window, 'requestAnimationFrame')];
+    return raf || function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+  })();
+
+  var ninja = document.querySelector("#play-ninja");
+  var cover = document.querySelector(".cover");
+
+  var ticking = false;
+  var transform;
+  var timer;
+
+  var mc = new Hammer.Manager(ninja);
+
+  mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
+  mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
+  mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
+  mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith([mc.get('pan'), mc.get('rotate')]);
+
+  mc.on("panstart panmove", onPan);
+  mc.on("rotatestart rotatemove", onRotate);
+  mc.on("pinchstart pinchmove", onPinch);
+  mc.on("swipe", onSwipe);
+
+  mc.on("hammer.input", function(ev) {
+    if (ev.isFinal) {
+      resetElement();
+    }
+  });
+
+  function resetElement() {
+    ninja.className = 'animate';
+    transform = {
+      translate: { x: 0, y: 0 },
+      scale: 1,
+      angle: 0,
+      rx: 0,
+      ry: 0,
+      rz: 0
+    };
+    requestElementUpdate();
+  }
+
+  function updateElementTransform() {
+    var value = [
+      'translate3d(' + transform.translate.x + 'px, ' +
+        transform.translate.y + 'px, 0)',
+      'scale(' + transform.scale + ', ' + transform.scale + ')',
+      'rotate3d('+ transform.rx +','+ transform.ry +','+
+        transform.rz +','+  transform.angle + 'deg)'
+    ];
+
+    value = value.join(" ");
+    ninja.style.webkitTransform = value;
+    ninja.style.mozTransform = value;
+    ninja.style.transform = value;
+    ticking = false;
+  }
+
+  function requestElementUpdate() {
+    if(!ticking) {
+      reqAnimationFrame(updateElementTransform);
+      ticking = true;
+    }
+  }
+
+  var max = Math.min(cover.offsetWidth / 2, cover.offsetHeight / 2);
+
+  function damp(x, max) {
+    var abs = max * (1 - Math.exp(-0.005 * Math.abs(x)));
+    return x > 0 ? abs : -abs;
+  }
+
+  function onPan(ev) {
+    ninja.className = '';
+    transform.translate = {
+      x: damp(ev.deltaX, max),
+      y: damp(ev.deltaY, max)
+    };
+
+    requestElementUpdate();
+  }
+
+  var initScale = 1;
+  function onPinch(ev) {
+    if(ev.type == 'pinchstart') {
+      initScale = transform.scale || 1;
+    }
+
+    ninja.className = '';
+    transform.scale = initScale * ev.scale;
+
+    requestElementUpdate();
+  }
+
+  var initAngle = 0;
+  function onRotate(ev) {
+    if(ev.type == 'rotatestart') {
+      initAngle = transform.angle || 0;
+    }
+
+    ninja.className = '';
+    transform.rz = 1;
+    transform.angle = initAngle + ev.rotation;
+
+    requestElementUpdate();
+  }
+
+  function onSwipe(ev) {
+    var angle = 50;
+    transform.ry = (ev.direction & Hammer.DIRECTION_HORIZONTAL) ? 1 : 0;
+    transform.rx = (ev.direction & Hammer.DIRECTION_VERTICAL) ? 1 : 0;
+    transform.angle = (ev.direction &
+      (Hammer.DIRECTION_RIGHT | Hammer.DIRECTION_UP)) ? angle : -angle;
+
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      resetElement();
+    }, 300);
+
+    requestElementUpdate();
+  }
+
+  resetElement();
+
+});
