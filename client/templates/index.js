a2dc9a9abb0602be5be3ce424a8a5b7cef9e4384
Staś Małolepszy <stas@mozilla.com>
2015-05-20 16:15:10 +0200

    add deep links for live and lines

--- client/templates/index.js
+++ client/templates/index.js
@@ -1,5 +1,13 @@
 Meteor.subscribe("reports");
 
-Template.index.rendered = function() {
+Template.index.onRendered(function() {
   $('ul.tabs').tabs();
-};
+});
+
+Template.index.events({
+  'click .tab a': function(evt) {
+    var route = evt.target.dataset.route;
+    window.history.replaceState(route, null, route);
+
+  }
+});
