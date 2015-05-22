3a9730c7cc11d3b72b3bf555d2fb54a119d38a05
Staś Małolepszy <stas@mozilla.com>
2015-05-22 11:37:43 +0200

    refactor card template

--- client/templates/card.js
+++ client/templates/card.js
@@ -22,25 +22,57 @@ hasActions = function(doc) {
   return canUpvote(doc) || canThank(doc);
 };
 
-
-Template.report.helpers({
-  canUpvote: canUpvote,
-  canRemove: canRemove,
-  canThank: canThank,
+Template.reportcard.helpers({
   isAuthor: isAuthor,
-  hasActions: hasActions,
-  icon: function(name) {
-    var repType = allReportTypes.filter(function(elem) {
-      return elem.name === name;
-    })[0];
-    return repType.icon;
-  }
+  hasActions: hasActions
 });
 
-Template.report.events({
+Template.reportcard.events({
   'click .card': function(evt) {
     $(evt.currentTarget).toggleClass('active');
   },
+  'click .upvote': function(evt) {
+    evt.stopImmediatePropagation();
+    if (canUpvote(this._id)) {
+      // Prevent future upvotes
+      Session.setPersistent(this._id + ' voted', 'up');
+      Meteor.call("upvoteReport", this._id);
+      Materialize.toast("Dzięki!", 2000);
+    }
+  },
+  'click .downvote': function(evt) {
+    evt.stopImmediatePropagation();
+    if (canUpvote(this._id)) {
+      Session.setPersistent(this._id + ' voted', 'down');
+      Meteor.call("downvoteReport", this._id);
+      Materialize.toast("Dzięki!", 2000);
+    }
+  },
+  'click .remove': function(evt) {
+    evt.stopImmediatePropagation();
+    if (canRemove(this._id, this.createdAt)) {
+      Session.clear(this._id + ' created', true);
+      Meteor.call("removeReport", this._id);
+      Materialize.toast("Puff!", 2000);
+    }
+  },
+  'click .thank': function(evt) {
+    evt.stopImmediatePropagation();
+    if (canThank(this._id)) {
+      Session.setPersistent(this._id + ' thanked', true);
+      Meteor.call("thankReport", this._id);
+      Materialize.toast("Podziękowanie wysłane!", 2000);
+    }
+  },
+});
+
+Template.cardactions.helpers({
+  canUpvote: canUpvote,
+  canRemove: canRemove,
+  canThank: canThank
+});
+
+Template.cardactions.events({
   'click .upvote': function(evt) {
     evt.stopImmediatePropagation();
     if (canUpvote(this._id)) {
