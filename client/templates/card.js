aa432af014bb2b9debc882e1cd1ca650a4eda3b9
Staś Małolepszy <stas@mozilla.com>
2015-05-22 15:22:12 +0200

    refactor feed cards

--- client/templates/card.js
+++ client/templates/card.js
@@ -13,13 +13,8 @@ canUpvote = function(doc) {
     false : !isAuthor(doc) && Session.equals(doc + ' voted', undefined);
 };
 
-canRemove = function(doc, createdAt) {
-  return isAuthor(doc) &&
-    Chronos.currentTime(5000) - new Date(createdAt) < 1000 * 30;
-};
-
 hasActions = function(doc) {
-  return canUpvote(doc) || canThank(doc);
+  return canUpvote(doc) && canThank(doc);
 };
 
 function relativeTime(time) {
@@ -35,54 +30,37 @@ function relativeTime(time) {
   }
 }
 
-Template.reportcard.helpers({
-  isAuthor: isAuthor,
-  hasActions: hasActions,
-  relativeTime: relativeTime
-});
+function howMany(votes) {
+  var trans = votes > 0 ? translations.confirms : translations.clears;
+  var num = Math.abs(votes);
+  return num + ' ' + trans[plural(Math.abs(num))];
+}
 
-Template.reportcard.events({
-  'click .card-content.has-actions': function(evt) {
+var cardHelpers = {
+  hasActions: hasActions,
+  relativeTime: relativeTime,
+  howMany: function(confirms, clears) {
+    return [confirms, clears].filter(
+      function(elem) { return elem !== 0; }).map(
+        howMany).join(', ');
+  }
+};
+
+var cardEvents = {
+  'click .card-content.pointer': function(evt) {
     $(evt.currentTarget).parent().toggleClass('active');
   },
-  'click .upvote': function(evt) {
-    evt.stopImmediatePropagation();
-    if (canUpvote(this._id)) {
-      // Prevent future upvotes
-      Session.setPersistent(this._id + ' voted', 'up');
-      Meteor.call("upvoteReport", this._id);
-      Materialize.toast("Dzięki!", 2000);
-    }
-  },
-  'click .downvote': function(evt) {
-    evt.stopImmediatePropagation();
-    if (canUpvote(this._id)) {
-      Session.setPersistent(this._id + ' voted', 'down');
-      Meteor.call("downvoteReport", this._id);
-      Materialize.toast("Dzięki!", 2000);
-    }
-  },
-  'click .remove': function(evt) {
-    evt.stopImmediatePropagation();
-    if (canRemove(this._id, this.createdAt)) {
-      Session.clear(this._id + ' created', true);
-      Meteor.call("removeReport", this._id);
-      Materialize.toast("Puff!", 2000);
-    }
-  },
-  'click .thank': function(evt) {
-    evt.stopImmediatePropagation();
-    if (canThank(this._id)) {
-      Session.setPersistent(this._id + ' thanked', true);
-      Meteor.call("thankReport", this._id);
-      Materialize.toast("Podziękowanie wysłane!", 2000);
-    }
-  },
-});
+};
+
+Template.reportcard.helpers(cardHelpers);
+Template.reportcard.events(cardEvents);
+
+Template.feedcard.helpers(cardHelpers);
+Template.feedcard.events(cardEvents);
 
 Template.cardactions.helpers({
+  isAuthor: isAuthor,
   canUpvote: canUpvote,
-  canRemove: canRemove,
   canThank: canThank
 });
 
@@ -106,7 +84,7 @@ Template.cardactions.events({
   },
   'click .remove': function(evt) {
     evt.stopImmediatePropagation();
-    if (canRemove(this._id, this.createdAt)) {
+    if (isAuthor(this._id)) {
       Session.clear(this._id + ' created', true);
       Meteor.call("removeReport", this._id);
       Materialize.toast("Puff!", 2000);
