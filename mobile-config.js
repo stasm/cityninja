40d01d389db5343825a14bf40bd4278a48a4d2f2
Staś Małolepszy <stas@mozilla.com>
2015-06-14 21:26:36 +0200

    cordova prefs

--- mobile-config.js
+++ mobile-config.js
@@ -14,3 +14,8 @@ App.icons({
   'android_hdpi': 'public/android/hdpi.png',
   'android_xhdpi': 'public/android/xhdpi.png',
 });
+
+App.setPreference('Fullscreen', false);
+App.setPreference('Orientation', 'portrait');
+App.setPreference('BackgroundColor', '0xff7E57C2');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
