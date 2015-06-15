f6e356536cb36a37ca70997cf1d6ad6ddf9ff998
Staś Małolepszy <stas@mozilla.com>
2015-06-15 14:36:37 +0200

    add video

--- server/policy.js
+++ server/policy.js
@@ -1,3 +1,4 @@
 BrowserPolicy.content.allowOriginForAll("http://meteor.local");
 BrowserPolicy.content.allowOriginForAll("http://analytics.trumba.pl");
 BrowserPolicy.content.allowOriginForAll("https://developer.android.com");
+BrowserPolicy.content.allowOriginForAll("https://www.youtube-nocookie.com");
