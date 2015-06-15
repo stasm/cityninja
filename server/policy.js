56d29e4279cf2db20869a0454ee13e366d884d81
Staś Małolepszy <stas@mozilla.com>
2015-06-15 05:20:31 +0200

    fix csp for images

--- server/policy.js
+++ server/policy.js
@@ -1,1 +1,3 @@
 BrowserPolicy.content.allowOriginForAll("http://meteor.local");
+BrowserPolicy.content.allowOriginForAll("http://analytics.trumba.pl");
+BrowserPolicy.content.allowOriginForAll("https://developer.android.com");
