1bc128fbfdcce20ce719f49e2e6718303a39ffcb
Staś Małolepszy <stas@mozilla.com>
2015-06-17 09:49:04 +0200

    quickadd.{html,js}

--- client/templates/index.js
+++ client/templates/index.js
@@ -2,17 +2,6 @@ Template.index.helpers({
   showFavInfo: function() {
     return viewingFavs() && !hasFavs();
   },
-  reportTypes: function() {
-    return reportTypes.common;
-  }
-});
-
-Template.index.events({
-  'click .btn-floating': function(evt) {
-    evt.currentTarget.parentNode.classList.remove('no-anim');
-    evt.currentTarget.parentNode.classList.toggle('collapsed');
-    evt.currentTarget.parentNode.classList.toggle('expanded');
-  }
 });
 
 Template.tabs.onRendered(function() {
---
+++ client/templates/quickadd.js
@@ -0,0 +1,14 @@
+Template.fab.helpers({
+  reportTypes: function() {
+    return reportTypes.common;
+  }
+});
+
+Template.fab.events({
+  'click .btn-floating': function(evt) {
+    evt.currentTarget.parentNode.classList.remove('no-anim');
+    evt.currentTarget.parentNode.classList.toggle('collapsed');
+    evt.currentTarget.parentNode.classList.toggle('expanded');
+  }
+});
+
