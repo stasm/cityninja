4b107714b95f89059a33b9c2ec0a46ed98da6bc3
Staś Małolepszy <stas@mozilla.com>
2015-06-19 11:58:49 +0200

    a simple implementation of achievements

--- client/templates/profile.js
+++ client/templates/profile.js
@@ -11,4 +11,80 @@ Template.profile.helpers({
     return translations[key][plural(num)];
   },
   numVotes: numVotes.bind(null, ' i '),
+  achievements: function() {
+    var completed = [];
+    var user = Meteor.user();
+
+    if (user.profile.favs.length > 0) {
+      completed.push({
+        name: 'Ulubieniec',
+        desc: 'Masz przynajmniej jedną linię w ulubionych.'
+      });
+    }
+
+    if (user.incoming.thanks >= 1) {
+      completed.push({
+        name: '1 podziękowanie',
+        desc: 'Twoje zgłoszenia zebrały 1 podziękowanie.'
+      });
+    }
+
+    if (user.incoming.thanks >= 5) {
+      completed.push({
+        name: '5 podziękowań',
+        desc: 'Twoje zgłoszenia zebrały 5 podziękowań.'
+      });
+    }
+
+    if (user.incoming.thanks >= 10) {
+      completed.push({
+        name: '10 podziękowań',
+        desc: 'Twoje zgłoszenia zebrały 10 podziękowań.'
+      });
+    }
+
+    if (user.incoming.thanks >= 50) {
+      completed.push({
+        name: '50 podziękowań',
+        desc: 'Twoje zgłoszenia zebrały 50 podziękowań.'
+      });
+    }
+
+    if (user.incoming.confirms >= 1) {
+      completed.push({
+        name: '1 potwierdzenie',
+        desc: 'Twoje zgłoszenia zostały potwierdzone 1 raz.'
+      });
+    }
+
+    if (user.incoming.confirms >= 5) {
+      completed.push({
+        name: '5 potwierdzeń',
+        desc: 'Twoje zgłoszenia zostały potwierdzone 5 razy.'
+      });
+    }
+
+    if (user.incoming.confirms >= 10) {
+      completed.push({
+        name: '10 potwierdzeń',
+        desc: 'Twoje zgłoszenia zostały potwierdzone 10 razy.'
+      });
+    }
+
+    if (user.incoming.confirms >= 50) {
+      completed.push({
+        name: '50 potwierdzeń',
+        desc: 'Twoje zgłoszenia zostały potwierdzone 50 razy.'
+      });
+    }
+
+    return completed;
+
+  }
+});
+
+Template.profile.events({
+  'click .collapsible-header': function(evt) {
+    evt.currentTarget.classList.toggle('active');
+  }
 });
