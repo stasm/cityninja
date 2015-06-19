f63c2c278cdb0dfc3634fc0a901adfd1d9bf8ea1
Staś Małolepszy <stas@mozilla.com>
2015-06-19 12:23:28 +0200

    icons

--- client/templates/profile.js
+++ client/templates/profile.js
@@ -18,6 +18,7 @@ Template.profile.helpers({
     if (user.profile.favs.length > 0) {
       completed.push({
         name: 'Ulubieniec',
+        icon: 'mdi-action-favorite amber-text',
         desc: 'Masz przynajmniej jedną linię w ulubionych.'
       });
     }
@@ -25,6 +26,7 @@ Template.profile.helpers({
     if (user.incoming.thanks >= 1) {
       completed.push({
         name: '1 podziękowanie',
+        icon: 'mdi-action-thumb-up amber-text',
         desc: 'Twoje zgłoszenia zebrały 1 podziękowanie.'
       });
     }
@@ -32,6 +34,7 @@ Template.profile.helpers({
     if (user.incoming.thanks >= 5) {
       completed.push({
         name: '5 podziękowań',
+        icon: 'mdi-action-thumb-up amber-text',
         desc: 'Twoje zgłoszenia zebrały 5 podziękowań.'
       });
     }
@@ -39,6 +42,7 @@ Template.profile.helpers({
     if (user.incoming.thanks >= 10) {
       completed.push({
         name: '10 podziękowań',
+        icon: 'mdi-action-thumb-up amber-text',
         desc: 'Twoje zgłoszenia zebrały 10 podziękowań.'
       });
     }
@@ -46,6 +50,7 @@ Template.profile.helpers({
     if (user.incoming.thanks >= 50) {
       completed.push({
         name: '50 podziękowań',
+        icon: 'mdi-action-thumb-up amber-text',
         desc: 'Twoje zgłoszenia zebrały 50 podziękowań.'
       });
     }
@@ -53,6 +58,7 @@ Template.profile.helpers({
     if (user.incoming.confirms >= 1) {
       completed.push({
         name: '1 potwierdzenie',
+        icon: 'mdi-action-grade amber-text',
         desc: 'Twoje zgłoszenia zostały potwierdzone 1 raz.'
       });
     }
@@ -60,6 +66,7 @@ Template.profile.helpers({
     if (user.incoming.confirms >= 5) {
       completed.push({
         name: '5 potwierdzeń',
+        icon: 'mdi-action-grade amber-text',
         desc: 'Twoje zgłoszenia zostały potwierdzone 5 razy.'
       });
     }
@@ -67,6 +74,7 @@ Template.profile.helpers({
     if (user.incoming.confirms >= 10) {
       completed.push({
         name: '10 potwierdzeń',
+        icon: 'mdi-action-grade amber-text',
         desc: 'Twoje zgłoszenia zostały potwierdzone 10 razy.'
       });
     }
@@ -74,6 +82,7 @@ Template.profile.helpers({
     if (user.incoming.confirms >= 50) {
       completed.push({
         name: '50 potwierdzeń',
+        icon: 'mdi-action-grade amber-text',
         desc: 'Twoje zgłoszenia zostały potwierdzone 50 razy.'
       });
     }
