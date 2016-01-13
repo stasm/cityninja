c394bd01f432661bdd00d72eb879f7ad038fc2fc
Staś Małolepszy <stas@mozilla.com>
2016-01-13 15:41:50 +0100

    nickname help text

--- client/templates/drawer.js
+++ client/templates/drawer.js
@@ -1,5 +1,20 @@
+Template.drawer.onRendered(() => {
+  $('.mdl-collapse__content').each(function() {
+    const content = $(this);
+    content.css('margin-top', -content.outerHeight(true));
+  })
+});
+
 Template.drawer.helpers({
   nickname() {
     return Meteor.user().profile.nickname;
+  }
+});
+
+Template.drawer.events({
+  ['click .mdl-collapse__button'](evt) {
+    $(evt.currentTarget)
+      .parents('.mdl-collapse')
+      .toggleClass('mdl-collapse--opened');
   }
 });
