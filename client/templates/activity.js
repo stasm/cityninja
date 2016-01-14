a309fbefce10c7bee929560759deb44c1ebc8288
Staś Małolepszy <stas@mozilla.com>
2016-01-14 11:20:12 +0100

    activity

--- client/templates/activity.js
+++ client/templates/activity.js
@@ -1,33 +1,39 @@
-Template.profile.onCreated(trackPageView);
+Template.activity.onCreated(trackPageView);
+Template.activity.onRendered(() => {
+  $('.mdl-collapse__content').each(function() {
+    const content = $(this);
+    content.css('margin-top', -content.outerHeight(true));
+  })
+});
 
 function total(dir, type) {
   var user = Meteor.user();
   return user ? user[dir][type].length : 0;
 }
 
-Template.profile.helpers({
+Template.activity.helpers({
   all: total.bind(null, 'outgoing', 'reports-created'),
   thanks: total.bind(null, 'incoming', 'reports-thanked'),
   upvotes: total.bind(null, 'incoming', 'reports-upvoted'),
   downvotes: total.bind(null, 'incoming', 'reports-downvoted'),
-  plural: function(key, num) {
+  plural(key, num) {
     return translations[key][plural(num)];
   },
-  numVotes: function() {
+  numVotes() {
     return getVotesString(
       '', ' i ', total('incoming', 'reports-upvoted'),
       total('incoming', 'reports-downvoted'));
   },
-  achievements: function() {
+  achievements() {
     var user = Meteor.user();
     return user ? getAchievements(user) : [];
   }
 });
 
-Template.profile.events({
-  'click .collapsible-item': function(evt) {
-    var item= evt.currentTarget;
-    item.classList.toggle('active');
-    item.querySelector('.collapsible-header').classList.toggle('active');
+Template.activity.events({
+  ['click .mdl-collapse__button'](evt) {
+    $(evt.currentTarget)
+      .parents('.mdl-collapse')
+      .toggleClass('mdl-collapse--opened');
   }
 });
