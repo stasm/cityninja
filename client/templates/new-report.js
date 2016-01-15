e1ebd41e63d2dfbc020facb1c10356f244b6158f
Staś Małolepszy <stas@mozilla.com>
2016-01-15 22:44:33 +0100

    fab + simple new report screen

--- client/templates/new-report.js
+++ client/templates/new-report.js
@@ -1,6 +1,6 @@
 Template.newReport.onCreated(trackPageView);
 Template.newReport.onRendered(function() {
-  makeTagInput('input#stop');
+  // makeTagInput('input#stop');
 });
 
 Template.newReport.helpers({
@@ -10,24 +10,16 @@ Template.newReport.helpers({
 });
 
 Template.newReport.events({
-  'click .understood': function(evt) {
+  'click .nj-new-report__understood': function(evt) {
     evt.preventDefault();
     Session.setPersistent('user has seen the new report hint', true);
-  },
-  'click input#stop-display': function(evt) {
-    evt.preventDefault();
-    $('#pick-stop').openModal();
-  },
-  'click .close-modal': function(evt) {
-    evt.preventDefault();
-    $('#pick-stop').closeModal();
   },
   'submit form': function(evt) {
     evt.preventDefault();
     Meteor.call(
       'createReport',
-      evt.target.text.value,
-      evt.target.tags.value.split(',').filter(nonEmpty));
+      evt.target['new-report-text'].value);
+      // evt.target['new-report-tags'].value.split(',').filter(nonEmpty));
     queuedToasts.push(pickRandom(toasts.created));
     Router.go('feed.all');
   },
